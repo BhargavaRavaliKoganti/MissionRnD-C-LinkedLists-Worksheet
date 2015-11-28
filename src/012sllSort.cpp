5cdbd89c052e9edaa13c696f75a4c27e2599092c
ravali <ravali.888@gmail.com>
2015-11-28 20:13:54 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -22,5 +22,17 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
-	
+	struct node *cur;
+	struct node *ptr;
+	int temp;
+	for (cur = head; cur->next != NULL; cur = cur->next){
+		for (ptr = cur->next; ptr != NULL; ptr = ptr->next){
+			if (cur->data > ptr->data){
+				temp = cur->data;
+				cur->data = ptr->data;
+				ptr->data = temp;
+			}
+		}
+	}
+	head = cur;
 }
